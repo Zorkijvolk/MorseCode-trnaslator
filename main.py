8cf7257abda2736ac2b47008af50bdbbb1d7e076
Zorkijvolk <zorkijvolkmainkraft@gmail.com>
2024-01-21 19:36:04 +0300

    Update main.py
    3

--- main.py
+++ main.py
@@ -9,22 +9,35 @@ MorseCode = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..
              'п': '.--.', 'р': '.-.', 'с': '...', 'т': '-', 'у': '..-', 'ф': '..-.', 'х': '....', 'ц': '-.-.',
              'ч': '---.', 'ш': '----', 'щ': '--.-', 'ы': '-.--', 'ь': '-..-', 'э': '..-..', 'ю': '..--', 'я': '.-.-'
              }
+
+
 def decode_from_morse(code):
     morse_code_reverse = {v: k for k, v in MorseCode.items()}
     decoded_text = ''
-    for morse_char in code:
-        if morse_char in morse_code_reverse:
+    x61 = code.split('   ')
+    x16 = ' # '.join(x61)
+    for morse_char in x16.split():
+        if morse_char == '#':
+            decoded_text += ' '
+        elif morse_char in morse_code_reverse:
             decoded_text += morse_code_reverse[morse_char]
     return decoded_text
 
 
 def encode_to_morse(text):
+    encoded_message = []
+    for char in text.lower():
+        if char in MorseCode:
+            encoded_message.append(MorseCode[char])
+        elif char == ' ':
+            encoded_message.append(' ')
+    return ' '.join(encoded_message)
 
 
 def main():
     while True:
         print('''Приветствую. Я могу помоч вам закодировать или декодировать азбуку морзе. Введите слово "кодировать"
-        или "декодировать"''')
+        или "декодировать". Что бы закончить работу введите "стоп" слова должны разделятся тремя пробелами''')
         a = input()
         if a == 'кодировать':
             print('Введите текст')
@@ -34,3 +47,11 @@ def main():
             print('Введите код')
             b = input()
             print(decode_from_morse(b))
+        elif a == 'стоп':
+            print('Завершение работы')
+            break
+        else:
+            print('Программа не может распознать ваш ответ попробуйте снова')
+
+
+main()
